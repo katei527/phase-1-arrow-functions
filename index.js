a5197755b223821a233449f1a1868c38827fc5bf
Katei <kateievans28@gmail.com>
2025-03-11 22:28:01 +0300

    arroow-functions

---
+++ index.js
@@ -0,0 +1,14 @@
+const a = 2000
+const b = 100
+
+function divide() {
+     return (a/b);
+}
+console.log(divide(a/b)); 
+
+let x = 2
+const square = multiply => x * 2
+
+const y = 3
+const z = 4
+const add = addition => y + z
